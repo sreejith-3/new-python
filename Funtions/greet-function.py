8b16bf305d6d5870459b2d5129e92ae837700505
unknown <sreejith.cloudme@gmail.com>
2024-08-19 22:07:02 +0530

    all-things

---
+++ Funtions/greet-function.py
@@ -0,0 +1,8 @@
+def greet(name):
+    """
+    This function greets the person whose name is passed as an argument.
+    """
+    print(f"Hello, {name}!")
+
+greet("Sreejith Balakrishnan")
+greet("Alice")
