12befb05c46b88a9c6ae03072e8d8d9162dc0b0f
unknown <sreejith.cloudme@gmail.com>
2024-08-19 22:31:45 +0530

    nope-new

---
+++ List/access-list-items.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist[0])
+print(thislist[1])
+print(thislist[2])
+print("===================================")
+print(thislist[-1])
+print(thislist[-2])
+print(thislist[-3])
+print("===================================")
+thislist1 = ["apple", "banana", "cherry", "orange", "kiwi", "melon", "mango"]
+print(thislist1[2:5])
---
+++ List/add-item-to-list.py
@@ -0,0 +1,4 @@
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+#thislist.append("kiwi", "gauva")  ## It DOES NOT take two items
+print(thislist)
